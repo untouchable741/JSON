6d36de889427c604e15f13f805cd2e337143e305
Tai Vuong <vhuutai@gmail.com>
2018-03-23 21:38:04 +0700

    Init project

---
+++ JSON/Source/JSON.swift
@@ -0,0 +1,13 @@
+//
+//  JSON.swift
+//  JSON
+//
+//  Created by Tai Vuong on 3/23/18.
+//  Copyright © 2018 Huu Tai Vuong. All rights reserved.
+//
+
+import Foundation
+
+public struct JSON {
+    public init() { debugPrint("Hello JSON") }
+}
